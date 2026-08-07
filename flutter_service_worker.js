a27b9279d4ccaa2c4af2362d269e010659edaa7b
zyrridian <zyrridian@users.noreply.github.com>
2026-08-07 10:56:33 +0000

    deploy: 5ef9390d3d9584fd78290be599521ec9cee77a4d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "65e02f6fb55b8d5ee8df6298d8e329a9",
+"flutter_bootstrap.js": "3ce5830989b164ba301300fb77da2f2d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
